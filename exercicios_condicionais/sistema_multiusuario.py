178892fc2e25538e2ced46d20c4088c5dcd63b18
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-05-19 09:16:55 -0300

    Update sistema_multiusuario.py

--- exercicios_condicionais/sistema_multiusuario.py
+++ exercicios_condicionais/sistema_multiusuario.py
@@ -1,4 +1,4 @@
-#Cadastrar até 3 usuários
+#Cadastrar 3 usuários
 print("----------BEM-VINDO(A)----------")
 print("Cadastre-se\n")
 
@@ -29,100 +29,101 @@ print()
 
 #Login
 if opçao == "1":
-  print("----------BEM-VINDO(A) DE VOLTA----------")
-  print("Entre na sua conta\n")
-  tentativa_usuario = input("Usuário: ")
-  tentativa_senha = input("Senha: ")
-  print()
-  #Usuário 1
-  if tentativa_usuario == usuario1:
-    if tentativa_senha == senha1:
-      print(f"Acesso permitido.\nBem-vindo(a), {usuario1}.")
-    else:
-      print("Senha incorreta, tente novamente (2 tentativas restantes)")
-      tentativa_senha = input("Senha: ")
-      print()
-      if tentativa_senha == senha1:
-        print(f"Acesso permitido.\nBem-vindo(a), {usuario1}.")
-      else:
-        print("Senha incorreta, tente novamente (1 tentativa restante)")
-        tentativa_senha = input("Senha: ")
-        print()
+    print("----------BEM-VINDO(A) DE VOLTA----------")
+    print("Entre na sua conta\n")
+    tentativa_usuario = input("Usuário: ")
+    tentativa_senha = input("Senha: ")
+    print()
+    #Usuário 1
+    if tentativa_usuario == usuario1:
         if tentativa_senha == senha1:
-           print(f"Acesso permitido.\nBem-vindo(a), {usuario1}.")
+            print(f"Acesso permitido.\nBem-vindo(a), {usuario1}.")
         else:
-           print("Senha incorreta, usuário bloqueado. Reinicie o sistema.")
-  #Usuário 2
-  elif tentativa_usuario == usuario2:
-    if tentativa_senha == senha2:
-      print(f"Acesso permitido.\nBem-vindo(a), {usuario2}.")
-    else:
-      print("Senha incorreta, tente novamente (2 tentativas restantes)\n")
-      tentativa_senha = input("Senha: ")
-      print()
-      if tentativa_senha == senha2:
-        print(f"Acesso permitido.\nBem-vindo(a), {usuario2}.")
-      else:
-        print("Senha incorreta, tente novamente (1 tentativa restante)\n")
-        tentativa_senha = input("Senha: ")
-        print()
+            print("Senha incorreta, tente novamente (2 tentativas restantes)")
+            tentativa_senha = input("Senha: ")
+            print()
+            if tentativa_senha == senha1:
+                print(f"Acesso permitido.\nBem-vindo(a), {usuario1}.")
+            else:
+                print("Senha incorreta, tente novamente (1 tentativa restante)")
+                tentativa_senha = input("Senha: ")
+                print()
+                if tentativa_senha == senha1:
+                    print(f"Acesso permitido.\nBem-vindo(a), {usuario1}.")
+                else:
+                    print("Senha incorreta, usuário bloqueado. Reinicie o sistema.")
+    #Usuário 2
+    elif tentativa_usuario == usuario2:
         if tentativa_senha == senha2:
-           print(f"Acesso permitido.\nBem-vindo(a), {usuario2}.")
+            print(f"Acesso permitido.\nBem-vindo(a), {usuario2}.")
         else:
-           print("Senha incorreta, usuário bloqueado. Reinicie o sistema.")
-  #Usuário 3
-  elif tentativa_usuario == usuario3:
-    if tentativa_senha == senha3:
-      print(f"Acesso permitido.\nBem-vindo(a), {usuario3}.")
-    else:
-      print("Senha incorreta, tente novamente (2 tentativas restantes)\n")
-      tentativa_senha = input("Senha: ")
-      print()
-      if tentativa_senha == senha3:
-        print(f"Acesso permitido.\nBem-vindo(a), {usuario3}.")
-      else:
-        print("Senha incorreta, tente novamente (1 tentativa restante)\n")
-        tentativa_senha = input("Senha: ")
-        print()
+            print("Senha incorreta, tente novamente (2 tentativas restantes)\n")
+            tentativa_senha = input("Senha: ")
+            print()
+            if tentativa_senha == senha2:
+                print(f"Acesso permitido.\nBem-vindo(a), {usuario2}.")
+            else:
+                print("Senha incorreta, tente novamente (1 tentativa restante)\n")
+                tentativa_senha = input("Senha: ")
+                print()
+                if tentativa_senha == senha2:
+                   print(f"Acesso permitido.\nBem-vindo(a), {usuario2}.")
+                else:
+                   print("Senha incorreta, usuário bloqueado. Reinicie o sistema.")
+    #Usuário 3
+    elif tentativa_usuario == usuario3:
         if tentativa_senha == senha3:
-           print(f"Acesso permitido.\nBem-vindo(a), {usuario3}.")
+            print(f"Acesso permitido.\nBem-vindo(a), {usuario3}.")
         else:
-           print("Senha incorreta, usuário bloqueado. Reinicie o sistema.")
-  #Usuário inexistente
-  else:
-     print("Usuário não cadastrado. Acesso negado.")
+            print("Senha incorreta, tente novamente (2 tentativas restantes)\n")
+            tentativa_senha = input("Senha: ")
+            print()
+            if tentativa_senha == senha3:
+                print(f"Acesso permitido.\nBem-vindo(a), {usuario3}.")
+            else:
+                print("Senha incorreta, tente novamente (1 tentativa restante)\n")
+                tentativa_senha = input("Senha: ")
+                print()
+                if tentativa_senha == senha3:
+                    print(f"Acesso permitido.\nBem-vindo(a), {usuario3}.")
+                else:
+                    print("Senha incorreta, usuário bloqueado. Reinicie o sistema.")
+    #Usuário inexistente
+    else:
+        print("Usuário não cadastrado. Acesso negado.")
 #Recuperar senha
 elif opçao == "2":
-  print("----------RECUPERAÇÃO DE SENHA----------")
-  conta = input("De qual conta deseja recuperar a senha? ")
-  print()
-  #Usuário 1
-  if conta == usuario1:
-    tentativa_resp = input("Responda a pergunta secreta para recuperar a senha: Qual a sua comida preferida? ")
+    print("----------RECUPERAÇÃO DE SENHA----------")
+    conta = input("De qual conta deseja recuperar a senha? ")
     print()
-    if tentativa_resp == resp1:
-      print(f"Resposta correta.\nSenha: {senha1}")
+    #Usuário 1
+    if conta == usuario1:
+        tentativa_resp = input("Responda a pergunta secreta para recuperar a senha: Qual a sua comida preferida? ")
+        print()
+        if tentativa_resp == resp1:
+            print(f"Resposta correta.\nSenha: {senha1}")
+        else:
+            print(f"Resposta incorreta.")
+    #Usuário 2
+    elif conta == usuario2:
+        tentativa_resp = input("Responda a pergunta secreta para recuperar a senha: Qual a sua comida preferida? ")
+        print()
+        if tentativa_resp == resp2:
+            print(f"Resposta correta.\nSenha: {senha2}")
+        else:
+            print("Resposta incorreta.")
+    #Usuário 3
+    elif conta == usuario3:
+        tentativa_resp = input("Responda a pergunta secreta para recuperar a senha: Qual a sua comida preferida? ")
+        print()
+        if tentativa_resp == resp3:
+            print(f"Resposta correta.\nSenha: {senha3}")
+        else:
+            print("Resposta incorreta.")
+    #Usuário inexistente
     else:
-      print(f"Resposta incorreta.")
-  #Usuário 2
-  elif conta == usuario2:
-    tentativa_resp = input("Responda a pergunta secreta para recuperar a senha: Qual a sua comida preferida? ")
-    print()
-    if tentativa_resp == resp2:
-      print(f"Resposta correta.\nSenha: {senha2}")
-    else:
-      print("Resposta incorreta.")
-  #Usuário 3
-  elif conta == usuario3:
-    tentativa_resp = input("Responda a pergunta secreta para recuperar a senha: Qual a sua comida preferida? ")
-    print()
-    if tentativa_resp == resp3:
-      print(f"Resposta correta.\nSenha: {senha3}")
-    else:
-      print("Resposta incorreta.")
-  #Usuário inexistente
-  else:
-    print("Usuário não cadastrado.")
+        print("Usuário não cadastrado.")
 #Sair
 else:
   print("Você saiu.")
+  
