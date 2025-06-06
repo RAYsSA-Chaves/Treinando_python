3827611510afe6d66b37eeaaa63dea7ed0154f2b
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-06-06 10:18:37 -0300

    Update maior_n.py

--- exercicios_condicionais/maior_n.py
+++ exercicios_condicionais/maior_n.py
@@ -1,6 +1,8 @@
+#Ler dois números
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite um novo número: "))
 
+#Comparar para achar o maior
 if n1 > n2:
     print(f"{n1} é maior")
 elif n2 > n1:
