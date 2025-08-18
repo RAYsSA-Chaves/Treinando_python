8b018df2ddb555e32e8f88295adc200e6b301eaa
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-08-18 09:03:52 -0300

    Update tamagoshi.py

--- POO/tamagoshi/tamagoshi.py
+++ POO/tamagoshi/tamagoshi.py
@@ -16,29 +16,36 @@ class Tamagoshi:
     def alimentar(self, qtd):
         if (qtd >= 0) and (qtd <= 100):
             self.fome -= self.fome * (qtd/100)
+            mostrarInfo()
 
     def brincar(self, qtd):
         if (qtd >= 0) and (qtd <= 100):
             self.tedio -= self.tedio * (qtd/100)
+            mostrarInfo()
 
     def getHumor(self):
         return ((self.fome + self.tedio)/2)
+        mostrarInfo()
     
     def vida(self):
         if ((self.fome > 50 and self.fome <= 60)) or ((self.tedio > 50 and self.tedio <= 60)):
             self.saude -=10
+            mostrarInfo()
         elif ((self.fome > 60 and self.fome <= 60)) or ((self.tedio > 60 and self.tedio <= 60)):
             self.saude -=30
+            mostrarInfo()
         elif ((self.fome > 80 and self.fome <= 90)) or ((self.tedio > 80 and self.tedio <= 60)):
             self.saude -=50
+            mostrarInfo()
         elif (self.fome > 90) or (self.tedio > 90):
             print("Estou morrendo.......AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!!!")
+            mostrarInfo()
         elif (self.fome > 99) or (self.tedio > 99):
             self.saude = 0
             print("Seu bichinho morreu T_T") 
 
     def tempoPassando(self):
-        self.vida()
         self.idade += 0.2
         self.tedio += 2.5
         self.fome += 10
+        mostrarInfo()
